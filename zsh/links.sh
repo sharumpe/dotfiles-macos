19d42afd2f292f6c333aebb6de9d5b31e89654e9
Tim McLaughlin <mclaugtd@wwu.edu>
2026-05-12 08:44:01 -0700

    Accepting that my machine name doesn't get to be determined by me. As long as it's static. different zprofile includes for hosts and local un-tracked file for tokens. will eventually be built on-demand from 1password CLI.

--- zsh/links.sh
+++ zsh/links.sh
@@ -4,4 +4,3 @@ ln -fn -s ~/Documents/Projects/dotfiles/zsh/dot-zsh.d ~/.zsh.d
 ln -f -s ~/.zsh.d/zshrc ~/.zshrc
 ln -f -s ~/.zsh.d/zshrc.zni ~/.zshrc.zni
 ln -f -s ~/.zsh.d/zprofile ~/.zprofile
-ln -f -s ~/.zsh.d/zprofile-$(hostname -s) ~/.zprofile-local
