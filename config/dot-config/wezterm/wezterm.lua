b4f938c1f5d66e746df4d392a6404ad1072a604f
Tim McLaughlin <mrsharumpe@macwebb.com>
2026-05-13 22:30:32 -0700

    Adding back linux-compatible copy/paste keybinds. Making background pic path work for both platforms.

--- config/dot-config/wezterm/wezterm.lua
+++ config/dot-config/wezterm/wezterm.lua
@@ -9,7 +9,7 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Tokyo Night'
 config.hide_tab_bar_if_only_one_tab = true
 
-config.window_background_image = '/Users/Shared/Wallpapers/pink_sunset_bike_uhd.jpg'
+config.window_background_image = os.getenv("HOME") .. '/Pictures/Backgrounds/pink_bike_sunset_uhd.jpg'
 config.window_background_image_hsb = {
   brightness = 0.1
 }
