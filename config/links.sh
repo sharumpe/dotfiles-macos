c9f176d16b64369aed31d3b14f0dcc7763174f99
Tim McLaughlin <mrsharumpe@macwebb.com>
2026-05-15 14:57:00 -0700

    Removing htoprc as it's supposed to be os-specific. Adding bat. Redoing links.sh so that it only links parts we want to specifically manage.

--- config/links.sh
+++ config/links.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-ln -s ~/Documents/Projects/dotfiles/config/dot-config ~/.config
+ln -s ~/Documents/Projects/dotfiles/config/dot-config/bat ~/.config/bat
+ln -s ~/Documents/Projects/dotfiles/config/dot-config/qutebrowser ~/.config/qutebrowser
+ln -s ~/Documents/Projects/dotfiles/config/dot-config/wezterm ~/.config/wezterm
