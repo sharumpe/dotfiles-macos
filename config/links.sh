fa4a6540f1aa0d677e24e78499ff488bd9a84ad2
Tim McLaughlin <mclaugtd@wwu.edu>
2026-08-17 14:04:15 -0700

    tracking digrc changes.

--- config/links.sh
+++ config/links.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+ln -s ~/Documents/Projects/dotfiles/config/dot-config/dig/digrc ~/.digrc
 ln -s ~/Documents/Projects/dotfiles/config/dot-config/bat ~/.config/bat
 ln -s ~/Documents/Projects/dotfiles/config/dot-config/qutebrowser ~/.config/qutebrowser
 ln -s ~/Documents/Projects/dotfiles/config/dot-config/wezterm ~/.config/wezterm
